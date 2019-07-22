fc89d0717e0bf19db1e1f4a60b2417e041f45a32
veena863 <53148555+veena863@users.noreply.github.com>
2019-07-22 16:06:51 +0530

    Add files via upload

---
+++ practice01.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+y=20
+print(y)
+
+
+# In[2]:
+
+
+message="Hello Python World!"
+
+
+# In[3]:
+
+
+print(message)
+
+
+# In[ ]:
+
+
+#String Datatype
+name="veena raj"
+
+
+# In[4]:
+
+
+print(name.title())
+
+
+# In[5]:
+
+
+name="veena raj"
+print(name)
+
+
+# In[6]:
+
+
+print(name.title())
+
+
+# In[7]:
+
+
+print(name.upper())
+print(name.lower())
+
+
+# In[8]:
+
+
+first_name="Veena"
+last_name="Raj"
+full_name=first_name+last_name
+print(full_name)
+
+
+# In[9]:
+
+
+f_n=f"{first_name}{last_name}"
+print(f_n)
+
+
+# In[10]:
+
+
+print(f_n.title())
+print(f_n.upper())
+print(f_n.lower())
+
+
+# In[11]:
+
+
+print("python")
+print("python      ")
+
+
+# In[ ]:
+
+
+# for above also space is declared as a character which will occupy the space in the memory
+
+
+# In[12]:
+
+
+print(\tPython)
+
+
+# In[13]:
+
+
+print('\tPython')
+
+
+# In[ ]:
+
+
+#\t for tab space it adds 5 spaces before output
+
+
+# In[14]:
+
+
+print('python's')
+#when we are using ' problem with the single quotes in order to avoid this we are using double quotes.
+print("python's")
+
+
+# In[15]:
+
+
+print("python's")
+
+
+# In[16]:
+
+
+print("java/nc++/npython/npega")
+
+
+# In[17]:
+
+
+print("java\nc++\npython\npega")
+
+
+# In[18]:
+
+
+fav_lang="python    "
+print((fav_lang.rstrip())\n(fav_lang.lstrip()))
+
+
+# In[21]:
+
+
+fav_lang="python     "
+print(fav_lang.rstrip())
+
+
+# In[22]:
+
+
+fav_lang="      python     "
+print(fav_lang.strip())
+
+
+# In[ ]:
+
+
+
+
